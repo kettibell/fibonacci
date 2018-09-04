60e193e108caf26d491a9360b7d5fc6f0792f1ee
Kateryna Lukianova <kettibellya@gmail.com>
2018-09-04 20:32:32 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ function fibonacciFor(n) {
     document.write('<h1>1) First option (for):</h1>');
     document.write('Fibonacci numbers: <br>')
     let fib = [];
-    fib[0] = 0;
+    fib[0] = 1;
     fib[1] = 1;
     for (let i = 0; i <= n - 1; i++) {
         fib[i + 2] = fib[i] + fib[i + 1];
@@ -17,11 +17,11 @@ function fibonacciFor(n) {
 document.write(fibonacciFor(n));
 
 function fibonacciWhile(n) {
-    document.write('<h1>1) Second option (while):</h1>');
+    document.write('<h1>2) Second option (while):</h1>');
     document.write('Fibonacci numbers: <br>')
     let i = 0;
     let fib = [];
-    fib[0] = 0;
+    fib[0] = 1;
     fib[1] = 1;
     while (i < n) {
         fib[i + 2] = fib[i] + fib[i + 1];
@@ -32,3 +32,29 @@ function fibonacciWhile(n) {
     return '<h1>' + n + ' element is: ' + fib[n] + '</h1>';
 }
 document.write(fibonacciWhile(n));
+
+function fibonacciDoWhile(n) {
+    document.write('<h1>3) Third option (do while):</h1>');
+    document.write('Fibonacci numbers: <br>')
+    let i = 0;
+    let fib = [];
+    fib[0] = 1;
+    fib[1] = 1;
+    do {
+        fib[i + 2] = fib[i] + fib[i + 1];
+        fib[n] = fib[i];
+        document.write(fib[i] + '<br>');
+        i++;
+    } while (i < n);
+    return '<h1>' + n + ' element is: ' + fib[n] + '</h1>';
+}
+document.write(fibonacciDoWhile(n));
+
+let fib = 1;
+function fibonacciRec(n) {
+    if (n >= 3) {
+        return fibonacciRec(n - 1) + fibonacciRec(n - 2);
+    } return fib;
+}
+document.write('<h1>4) Firth option (Recursion):</h1>');
+document.write('<h1>' + n + ' element is: ' + fibonacciRec(n) + '</h1>');
